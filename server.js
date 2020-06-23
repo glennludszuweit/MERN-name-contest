59ee93a4747a19a67686921a5e48c285080af298
glenn <glenn.ludszuweit@gmail.com>
2020-06-23 22:45:22 +0200

    setup pug

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import apiRouter from './api';
+import path from 'path';
 
 import config from './config';
 const server = express();
 
-server.use(express.static('public'));
+server.set('view engine', 'pug');
+server.set('views', path.join(__dirname, 'views'));
+
+server.get('/', (req, res) => {
+  res.render('index');
+});
+
+server.use('/api', apiRouter);
+server.use(express.static(path.join(__dirname, 'public')));
 
 server.listen(config.port, () => {
   console.info(`SERVER running on port ${config.port}`);
