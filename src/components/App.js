3cfb721c23203fbda6ba8a00b8ee4467a9cd6be5
glenn <glenn.ludszuweit@gmail.com>
2020-06-24 14:45:17 +0200

    fetch data from api using axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Header from './Header';
 import ContestPreview from './ContestPreview';
 
 class App extends Component {
   state = {
     pageHeader: 'Naming Contests',
-    contests: [
-      {
-        id: 1,
-        categoryName: 'Business/Company',
-        contestName: 'Cognitive Building Bricks',
-      },
-      {
-        id: 2,
-        categoryName: 'Magazine/Newsletter',
-        contestName: 'Educating people about sustainable food production',
-      },
-      {
-        id: 3,
-        categoryName: 'Software Component',
-        contestName: 'Big Data Analytics for Cash Circulation',
-      },
-      {
-        id: 4,
-        categoryName: 'Website',
-        contestName: 'Free programming books',
-      },
-    ],
+    contests: [],
   };
 
-  componentDidMount() {}
+  componentDidMount() {
+    axios
+      .get('/api/contests')
+      .then((res) => {
+        this.setState({
+          contests: res.data.contests,
+        });
+      })
+      .catch(console.error);
+  }
 
   componentWillUnmount() {}
 
@@ -39,7 +28,7 @@ class App extends Component {
         <Header message={this.state.pageHeader} />
         <div>
           {this.state.contests.map((contest) => (
-            <ContestPreview {...contest} />
+            <ContestPreview key={contest.id} {...contest} />
           ))}
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import data from './devdata';
-
 import App from './components/App';
 
-ReactDOM.render(
-  <App contests={data.contests} />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
